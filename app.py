79d6094fc6a14a0411824478b7f97106fb38fb65
JamalM <jamalashur@gmail.com>
2019-04-12 15:12:44 -0500

    Added a login page and a redirect to the homepage  from the login page when username and password are admin

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_pymongo import PyMongo
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Online_Store"
@@ -13,10 +13,20 @@ animaltoGet = {'commonName' : "Chipmunk"}
 
 
 @app.route("/")
-def home_page():
+def index() :
     animal = collection.find_one(animaltoGet)
     print(animal)
     return render_template('index.html', animal=animal)
 
 
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
