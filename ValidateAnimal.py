9dcb1371ce50baa3b6879698f1d6eae777a043aa
ninexball <12503741+ninexball@users.noreply.github.com>
2019-04-27 21:43:45 -0500

    update animal validation

--- ValidateAnimal.py
+++ ValidateAnimal.py
@@ -7,8 +7,9 @@ If not delete the json files and images
 """
 
 from pathlib2 import Path
-from util import load_json
+from util import load_json, save_json
 import os
+from random import choice
 
 
 def main():
@@ -24,17 +25,24 @@ def main():
 
         if ("Taxonomy" not in animal or "ImageURL" not in animal) or \
                 "ImageURL" in animal and "_sm" not in animal["ImageURL"] or \
-                animal["CommonName"] in animals_found:
+                animal["ImageURL"] in animals_found:
             # print(animal["CommonName"], "Taxonomy" in animal, "ImageURL" in animal)
             # print(animal["ImageURL"])
 
             print(file)
             image_file = cwd / 'Static'/'Images'/animal["ImageURL"]
             print(image_file)
-            os.remove(file)
-            os.remove(image_file)
+            try:
+                os.remove(file)
+                os.remove(image_file)
+            except Exception as e:
+                print(e)
 
-        animals_found.append(animal["CommonName"])
+        animals_found.append(animal["ImageURL"])
+
+        if "Zone" not in animal:
+            animal["Zone"] = choice('ABCDEFGHIJK')
+            save_json(animal, file)
 
     print(sorted(list(animals_found)))
     print(len(set(animals_found)))
