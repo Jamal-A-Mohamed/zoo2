1b4b2090775a123c511fb598b992dde0233a49dc
ninexball <12503741+ninexball@users.noreply.github.com>
2019-05-06 23:00:57 -0500

    temp

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
-import bcrypt
 import os
+import re
+from functools import wraps
 from random import choice
+
+import bcrypt
+from bleach import clean
 from flask import Flask, redirect, render_template, request, session, url_for, abort, Response, json, make_response
 from flask_pymongo import PyMongo
 from markdown import markdown
 from werkzeug.utils import secure_filename
+
 from util import random_banner
-import re
-from bleach import clean
-from functools import wraps
 
 # regex for heading substitution (BriefSummary => Brief Summary)
 camel_re = re.compile(r'(?!^)(?=[A-Z])')
@@ -31,7 +33,7 @@ mongo = PyMongo(app)
 collection = mongo.db["animals"]
 
 
-static_site = "https://3.214.189.209"
+static_site = "https://ninth.site"
 
 
 # empty array
@@ -112,7 +114,7 @@ def index():
 @secure_headers
 @HSTS
 def glossary():
-    return render_template('glossary.html', animal_list=animal_list, category="animals")
+    return render_template('glossary.html', animal_list=animal_list, category="animals", static_site=static_site)
 
 
 @app.route("/reptiles")
@@ -120,56 +122,55 @@ def glossary():
 @HSTS
 def reptiles():
     reptile_list = get_animals_from_classification(level="Class", classification="Reptilia")
-    return render_template('glossary.html', animal_list=reptile_list, category="reptiles")
+    return render_template('glossary.html', animal_list=reptile_list, category="reptiles", static_site=static_site)
 
 @app.route("/birds")
 @secure_headers
 @HSTS
 def theword():
     bird_list = get_animals_from_classification(level="Class", classification="Aves")
-    return render_template('glossary.html', animal_list=bird_list, category="birds")
+    return render_template('glossary.html', animal_list=bird_list, category="birds", static_site=static_site)
 
 @app.route("/mammals")
 @secure_headers
 @HSTS
 def mammals():
     mammal_list = get_animals_from_classification(level="Phylum", classification="Chordata")
-    return render_template('glossary.html', animal_list=mammal_list, category="mammals")
+    return render_template('glossary.html', animal_list=mammal_list, category="mammals", static_site=static_site)
 
 
-@app.route("/autocomplete")
-def autcomplete() :
-    return render_template('autocomplete.html', animal_list=animal_list)
+
+@app.route("/results/<name_searched>")
+@secure_headers
+@HSTS
+def search_results(name_searched):
+    print(name_searched)
+    results_list = [animal['CommonName'] for animal in collection.find({"CommonName":{"$regex": f'.*({name_searched}).*'}})]
+    results_list += [animal['CommonName'] for animal in collection.find({"ScientificName":{"$regex": f'.*({name_searched}).*'}})]
+    print(results_list)
+    results_list = list(sorted(set(results_list)))
+    return render_template('glossary.html', animal_list=results_list, category="Search Results")
+
 
 @app.route('/search', methods=['POST', 'GET'])
 @secure_headers
 @HSTS
 def search():
     if request.method == 'POST':
-        return redirect(url_for('animal_page', animal_name=request.form['animalname']))
-
-#
-# @app.route('/autocomplete', methods=['GET'])
-# def autocomplete() :
-#     search = request.args.get('q')
-#     print(search)
-#
-#     if search and len(search) >= 3:
-#         results = [animal for animal in animal_list if search in animal]
-#         print(results)
-#         return jsonify(matching_results=results)
-#     return "NOTHING IN THE SEARCH BAR"
+        entry = request.form['animalname']
+        if entry in animal_list:
+            return redirect(url_for('animal_page', animal_name=entry))
+        else:
+            return redirect(url_for('search_results', name_searched=entry))
 
 
-
-@app.route('/animalname', methods=['GET'])
+@app.route('/autocomplete', methods=['GET'])
 def autocomplete() :
     search = request.args.get('animalname')
 
     app.logger.debug(search)
     print(search)
 
-    print(Response(json.dumps(NAMES)))
     return Response(json.dumps(animal_list), mimetype='application/json')
 
 
@@ -258,7 +259,7 @@ def register():
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert(
-                {'name': request.form['username'], 'password': hashpass, 'firstname': request.form['firstname'], \
+                {'name' : request.form['username'], 'password' : hashpass, 'firstname' : request.form['firstname'],
                  'lastname': request.form['lastname']})
             session['username'] = request.form['username']
 
@@ -267,7 +268,7 @@ def register():
         return render_template('register.html', error='<div class="alert alert-danger"> Username Already exists<strong></strong>\
                         </div>')
 
-    return render_template('register.html')
+    return render_template('register.html', static_site=static_site)
 
 
 @app.route('/edit/<animal_name>', methods=['POST', 'GET'])
@@ -362,7 +363,6 @@ def wiki_attribution(source_url):
 
 if __name__ == '__main__':
     app.secret_key = 'JR4WRBQUR6SDKuPTjrkCGBJ2UFF2TXxqhh'
-    app.run(ssl_context=('cert.pem','key.pem'), host="0.0.0.0", port=5002)
-    # app.run(host="0.0.0.0", port=5002)
-    # app.run(host="127.0.0.1", port=5000)
+    app.run(ssl_context=('fullchain.pem','privkey.pem'), host="0.0.0.0", port=5002)
+    # app.run(ssl_context=('fullchain.pem','privkey.pem'))
 
